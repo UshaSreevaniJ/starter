fe7d642db9be1a8277317d0707ebd4adb2c8cbc0
UshaSreevani Jonnalagadda <UshaSreevani.Jonnalagadda@Cerner.com>
2020-10-06 13:18:56 +0530

    Updates

---
+++ precache-manifest.f9dea04eff080f8e27eb1080a7f540af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed053cb335a6b5b6407c4936e9a33e4",
+    "url": "/starter/index.html"
+  },
+  {
+    "revision": "49bf469cf73b75230e6f",
+    "url": "/starter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fbc26ba8e5960c82c7e",
+    "url": "/starter/static/js/2.4c8a8079.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starter/static/js/2.4c8a8079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bf469cf73b75230e6f",
+    "url": "/starter/static/js/main.a639f3fd.chunk.js"
+  },
+  {
+    "revision": "66f8f131a8cba21fc918",
+    "url": "/starter/static/js/runtime-main.41791c99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starter/static/media/logo.5d5d9eef.svg"
+  }
+]);
