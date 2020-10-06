2cf0012cb4ce5dd2387e8d1f8f18f8ab415b3410
UshaSreevani Jonnalagadda <UshaSreevani.Jonnalagadda@Cerner.com>
2020-10-06 13:11:27 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Usha Sreevani Jonnalagadda
         </a>
       </header>
     </div>
